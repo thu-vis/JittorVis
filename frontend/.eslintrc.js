eb45ebd7ec6e83d7ce44e43373f3a6709d9bb3b2
ThuWangzw <wangzw834@163.com>
2021-10-05 15:42:39 +0800

    feat: add transform

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,30 +1,28 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  env: {
-    browser: true,
-  },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {
+    'env': {
+        'browser': true,
+        'es2021': true,
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'google',
+    ],
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module',
+    },
+    'plugins': [
+        'vue',
+    ],
+    'rules': {
     // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "indent": ["error", 4]
-  }
-}
+        'generator-star-spacing': 'off',
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'indent': ['error', 4],
+        'max-len': [
+            'error',
+            {'code': 150},
+        ],
+    },
+};
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import store from './store/store.js'
+import Vue from 'vue';
+import App from './App';
+import store from './store/store.js';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     store: store,
-    components: { App },
-    template: '<App/>'
-})
+    components: {App},
+    template: '<App/>',
+});
