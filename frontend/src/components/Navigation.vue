78e5c471b8a4f9217d8a6400bad40ee52b45acfb
zihan <nzh21@mails.tsinghua.edu.cn>
2021-10-08 16:26:07 +0800

    add tree layout

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -1,11 +1,16 @@
 <template>
 <svg id="navigation-tree">
-    <text>this is navigation</text>
+    <g class="container">
+        <g class="links"></g>
+        <g class="nodes"></g>
+    </g>
 </svg>
 </template>
 
 <script>
+/* eslint-disable */
 import { mapGetters, mapState } from 'vuex'
+import * as d3 from 'd3'
 
 export default {
     name: 'navigation',
@@ -16,13 +21,138 @@ export default {
         ...mapState([
             'focusID'
         ])
+    },
+    data(){
+        return {
+            _focusID:undefined,
+        }
+    },
+    watch:{
+        _focusID(val,val_0){
+            console.log("value changed",val)
+        }
+    },
+    created() {
+        const that=this
+        setTimeout(()=>{that.updateTree()},1000)
+        setTimeout(()=>{that._focusID="_model/layer4/";console.log("!")},2000)
+        setTimeout(()=>{that.updateTree()},3000)
+        setTimeout(()=>{that._focusID="_model/layer3/";console.log("!")},4000)
+        setTimeout(()=>{that.updateTree()},5000)
+    },
+    methods:{
+        updateTree(){
+            console.log("..update",this._focusID)
+            window.d3=d3
+            const width=600
+
+            // building tree
+            // required:  the first node must be the root
+            // required:  the id of the nodes must follow the pattern "fa/fa/fa/fa/id/"
+
+            let network_list=[]
+
+            let faID=undefined
+            for (let key in this.network)
+            {
+                const id=this.network[key].id
+                const fa=this.network[key].parent
+                if(!this._focusID)this._focusID=id
+                if(!faID)faID=this._focusID.slice(0,this._focusID.slice(0,-1).lastIndexOf('/'))+"/"
+                if(fa==this._focusID || fa==faID)
+                {
+                    network_list.push(
+                    {
+                        "id":id,
+                        "parent":fa
+                    })
+                }
+                else if(id==faID){
+                    network_list.push(
+                    {
+                        "id":id,
+                        "parent":undefined
+                    })
+                }
+            }
+            window.network_list=network_list
+            var root=d3.stratify().id(d=>d["id"]).parentId(d=>d["parent"])(network_list)
+            const tree = data => {
+                const root = d3.hierarchy(data);
+                root.dx = 10;
+                root.dy = width / (root.height + 1);
+                return d3.tree().nodeSize([root.dx, root.dy])(root);
+            }
+            root=tree(root)
+            window.root=root
+
+            // rendering
+            let x0 = Infinity;
+            let x1 = -x0;
+            root.each(d => {
+                if (d.x > x1) x1 = d.x;
+                if (d.x < x0) x0 = d.x;
+            });
+
+            const svg = d3.select("#navigation-tree")
+                .attr("viewBox", [0, 0, width, x1 - x0 + root.dx * 2]);
+            window.svg=svg
+
+            const g = svg.select("g.container")
+                .attr("transform", `translate(${root.dy / 3},${root.dx - x0})`)
+                
+            const link = g.select("g.links")
+                .attr("fill", "none")
+                .attr("stroke", "#555")
+                .attr("stroke-opacity", 0.6)
+                .attr("stroke-width", 1)
+            
+            .selectAll("path")
+                .data(root.links())
+                .join("path")
+                .attr("d", d3.linkHorizontal()
+                    .x(d => d.y)
+                    .y(d => d.x));
+            
+            const node = g.select("g.nodes")
+                .attr("stroke-linejoin", "round")
+                .attr("stroke-width", 3)
+                .selectAll("g")
+                .data(root.descendants())
+                .join("g")
+                .attr("transform", d => `translate(${d.y},${d.x})`);
+            
+            node.selectAll("text").remove()
+            node.selectAll("circle").remove()
+
+            node.append("circle")
+                .attr("fill", d => d.children ? "#555" : "#999")
+                .attr("r", 2.5);
+
+            
+
+            node.append("text")
+                .attr("dy", "0.31em")
+                .attr("x", d => d.children ? -6 : 6)
+                .attr("text-anchor", d => d.children ? "end" : "start")
+                .text(d => d.data.id.slice(d.parent && d.parent.data.id.length,-1))
+                .clone(true).lower()
+                .attr("stroke", "white");
+        }
     }
+    
 }
 </script>
 
 <style scoped>
 #navigation-tree {
-  width: 80%;
-  height: 80%;
+  width: 100%;
+  height: 100%;
+  margin:10px;
+}
+g.container
+{
+    font-family:"sans-serif";
+    font-size: 10;
 }
 </style>
