6719302b3d4cb5d0634b9ab434282b664785a0bc
ThuWangzw <wangzw834@163.com>
2021-10-13 16:33:06 +0800

    feat: add zoom in and zoom out for network view

--- frontend/src/components/Network.vue
+++ frontend/src/components/Network.vue
@@ -1,11 +1,23 @@
 <template>
-<vue-scroll :ops="scrollOptions">
-    <svg id="network-all" ref="networkAll" v-bind:style="{ height: height+'px' }">
-        <g id="network-main">
-            <network-layout :width="width" v-on:reheight='reheight'></network-layout>
-        </g>
-    </svg>
-</vue-scroll>
+<div id="network-all">
+    <div id="network-tools-bar">
+        <svg class="network-tools-bar-icon" @mouseenter="zoomInOpacity=hoverOpacity"
+            @mouseleave="zoomInOpacity=iconOpacity" :width="iconSize" :height="iconSize">
+            <image xlink:href="/static/images/zoomin.svg" @click="zoomIn" :width="iconSize" :height="iconSize" :opacity="zoomInOpacity"></image>
+        </svg>
+        <svg class="network-tools-bar-icon" @mouseenter="zoomOutOpacity=hoverOpacity"
+            @mouseleave="zoomOutOpacity=iconOpacity" :width="iconSize" :height="iconSize">
+            <image xlink:href="/static/images/zoomout.svg" @click="zoomOut" :width="iconSize" :height="iconSize" :opacity="zoomOutOpacity"></image>
+        </svg>
+    </div>
+    <div id="network-main-container">
+        <vue-scroll :ops="scrollOptions">
+            <div class="network-view-all">
+                <network-layout :scale="scale"></network-layout>
+            </div>
+        </vue-scroll>
+    </div>
+</div>
 </template>
 
 <script>
@@ -22,42 +34,58 @@ export default {
     },
     data: function() {
         return {
-            width: 0,
-            height: '100%',
+            iconSize: 20,
             scrollOptions: {
                 bar: {
                     background: '#c6bebe',
                 },
             },
+            hoverOpacity: 1,
+            iconOpacity: 0.5,
+            zoomInOpacity: 0.5,
+            zoomOutOpacity: 0.5,
+            scale: 1,
         };
     },
     methods: {
-        /**
-         * init svg size(width/height)
-         * @public
-         */
-        setsize: function() {
-            this.width = this.$refs.networkAll.clientWidth;
-            this.height = this.$refs.networkAll.clientHeight;
+        zoomIn: function() {
+            this.scale = Math.min(2, this.scale+0.1);
         },
-        /**
-         * change height of svg
-         * @param {number} height - new height
-         * @public
-         */
-        reheight: function(height) {
-            this.height = height;
+        zoomOut: function() {
+            this.scale = Math.max(0.5, this.scale-0.1);
         },
-    },
-    mounted: function() {
-        this.setsize();
     },
 };
 </script>
 
 <style>
 #network-all {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+}
+
+#network-tools-bar {
+    width: 100%;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+#network-main-container {
     width: 100%;
     height: 100%;
+    overflow: hidden;
+}
+
+.network-tools-bar-icon {
+    margin: 0 0 0 5px;
+    cursor: pointer;
+}
+
+.network-view-all {
+    display: flex;
+    justify-content: center;
 }
 </style>
