89277fb7bc48bd2b8a32ab0512aa03ad025fd358
ThuWangzw <wangzw834@163.com>
2021-10-15 17:15:49 +0800

    refactor: move transition duration to a global vue

---
+++ frontend/src/components/GlovalVar.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    data: function() {
+        return {
+            // transition duration
+            createDuration: 500,
+            updateDuration: 500,
+            removeDuration: 500,
+            delay: 100,
+        };
+    },
+};
+</script>
