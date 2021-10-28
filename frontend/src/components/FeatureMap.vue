f1c81424095a5ec6cd3c589bad7d0daf71a24a80
tianfy17 <tianfengyuan17@gmail.com>
2021-10-28 17:38:09 +0800

    feat: update featuremap view

--- frontend/src/components/FeatureMap.vue
+++ frontend/src/components/FeatureMap.vue
@@ -1,14 +1,17 @@
 <template>
     <div id="featuremaps">
-        <img class="featuremap" v-for="url in featureImages" :key="url" :src="url" />
+        <node-map v-for="nodeid in featureMapNodeIDs" :node-id="nodeid" :key="nodeid"
+            v-on:delete-id="deleteId"></node-map>
     </div>
 </template>
 
 <script>
 import {mapGetters} from 'vuex';
-import axios from 'axios';
+// import axios from 'axios';
+import NodeMap from './NodeMap.vue';
 
 export default {
+    components: {NodeMap},
     name: 'featuremap',
     computed: {
         ...mapGetters([
@@ -16,40 +19,58 @@ export default {
         ]),
     },
     watch: {
-        featureMapNodeID: function(newNodeID, oldNodeID) {
-            const that = this;
-            if (newNodeID != undefined) {
-                const store = this.$store;
-                axios.post(store.getters.URL_GET_FEATURE_INFO, {
-                    'branch': newNodeID,
-                }).then(function(response) {
-                    that.leafNode = response.data.leafID;
-                    if (that.leafNode === -1) {
-                        that.featureImages = [];
-                        return;
-                    }
-                    that.leafNodeShape =response.data.shape;
-                    const getFeature = store.getters.URL_GET_FEATURE;
-                    if (that.leafNodeShape.length===1) {
-                        // linear layer
-                        that.featureImages = [getFeature(that.leafNode, -1)];
-                    } else {
-                        // other layer
-                        that.featureImages = [];
-                        for (let i=0; i<that.leafNodeShape[0]; i++) {
-                            that.featureImages.push(getFeature(that.leafNode, i));
-                        }
-                    }
-                });
-            }
+        featureMapNodeIDs: function(newNodeIDs, oldNodeIDs) {
+            console.log('watched');
         },
+        featureMapNodeID: function(newNodeID, oldNodeID) {
+            console.log('setid', newNodeID, oldNodeID);
+            this.featureMapNodeIDs.push(newNodeID);
+        },
+        // featureMapNodeID: function(newNodeID, oldNodeID) {
+        //     const that = this;
+        //     if (newNodeID != undefined) {
+        //         const store = this.$store;
+        //         axios.post(store.getters.URL_GET_FEATURE_INFO, {
+        //             'branch': newNodeID,
+        //         }).then(function(response) {
+        //             that.leafNode = response.data.leafID;
+        //             if (that.leafNode === -1) {
+        //                 that.featureImages = [];
+        //                 return;
+        //             }
+        //             that.leafNodeShape =response.data.shape;
+        //             const getFeature = store.getters.URL_GET_FEATURE;
+        //             if (that.leafNodeShape.length===1) {
+        //                 // linear layer
+        //                 that.featureImages = [getFeature(that.leafNode, -1)];
+        //             } else {
+        //                 // other layer
+        //                 that.featureImages = [];
+        //                 for (let i=0; i<that.leafNodeShape[0]; i++) {
+        //                     that.featureImages.push(getFeature(that.leafNode, i));
+        //                 }
+        //             }
+        //         });
+        //     }
+        // },
     },
     data: function() {
         return {
             leafNode: null,
             leafNodeShape: [],
             featureImages: [],
+            featureMapNodeIDs: [],
         };
+    },
+    methods: {
+        deleteId(id) {
+            console.log('delete', id);
+            const index = this.featureMapNodeIDs.indexOf(id);
+            console.log(index);
+            if (index !== -1) {
+                this.featureMapNodeIDs.splice(index, 1);
+            }
+        },
     },
 };
 </script>
