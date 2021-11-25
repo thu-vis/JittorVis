425ca25a3658c4aa2687dddeb44b2c94bd60de7c
ThuWangzw <wangzw834@163.com>
2021-11-25 11:10:53 +0800

    refactor: move model view out of App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,44 +3,17 @@
     <div id="navigation">
       <div id="navi-title">JittorVis</div>
     </div>
-    <div id="content">
-      <div id="left">
-        <div id="confusion-matrix-container">
-          <span>—— Confusion Matrix ——</span>
-          <div class="dummy-container">
-            <confusion-matrix id="confusion-matrix"></confusion-matrix>
-          </div>
-        </div>
-        <div id="statistic-container">
-          <statistic v-for="item in Object.keys(statistic)" :key="item" :dataName="item" :statisticData="statistic[item]"></statistic>
-        </div>
-      </div>
-      <div id="middle">
-        <div id="tree-container">
-          <div id="network-container"><network></network></div>
-        </div>
-      </div>
-      <div id="featuremap-container">
-            <span>—— Features ——</span>
-            <vue-scroll :ops="scrollOptions">
-              <feature-map></feature-map>
-            </vue-scroll>
-        </div>
-    </div>
+    <model-view></model-view>
   </div>
 </template>
 
 <script>
-import Network from './components/Network.vue';
-import Statistic from './components/Statistic.vue';
-import FeatureMap from './components/FeatureMap.vue';
-import ConfusionMatrix from './components/ConfusionMatrix.vue';
-import {mapGetters} from 'vuex';
+import ModelView from './components/ModelView.vue';
 import axios from 'axios';
 
 // main vue component
 export default {
-    components: {Network, Statistic, FeatureMap, ConfusionMatrix},
+    components: {ModelView},
     name: 'App',
     mounted: function() {
         const store = this.$store;
@@ -54,20 +27,6 @@ export default {
                 store.commit('setConfusionMatrix', response.data);
                 console.log('confusion matrix data', store.getters.confusionMatrix);
             });
-    },
-    computed: {
-        ...mapGetters([
-            'statistic',
-        ]),
-    },
-    data: function() {
-        return {
-            scrollOptions: {
-                bar: {
-                    background: '#c6bebe',
-                },
-            },
-        };
     },
 };
 </script>
@@ -99,93 +58,4 @@ html, body, #app {
   font-size: 40px;
   margin: 0 0 0 20px;
 }
-
-#statistic-container {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-}
-
-#tree-container {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  overflow: hidden;
-  margin: 5px 5px 0 0;
-  border-top: 1px solid lightgray;
-}
-
-#featuremap-container {
-  width: 40%;
-  height: 100%;
-  border-left: 1px solid lightgray;
-}
-
-#content {
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  display: flex;
-}
-
-#left {
-  width: 20%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  border-right: 1px solid lightgray;
-  padding: 0 10px 0 0;
-}
-
-#middle {
-  width: 40%;
-  height: 100%;
-}
-
-#network-container {
-  width: 100%;
-  height: 100%;
-}
-
-#featuremap-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-#featuremap-container > span, #confusion-matrix-container > span {
-  font-family: Lucida Sans Typewriter;
-  font-weight: 400;
-  margin: 5px 0 5px 0;
-}
-
-#confusion-matrix-container {
-  position: relative;
-  width: 100%;
-  padding-top: 100%;
-}
-
-#confusion-matrix-container > span {
-  position:  absolute;
-  text-align: center;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-}
-
-.dummy-container {
-  position:  absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-}
-
 </style>
