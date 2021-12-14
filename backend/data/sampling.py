de53c4b30ee59d9934391c380df60356a28ccac8
ThuWangzw <wangzw834@163.com>
2021-12-14 14:16:33 +0800

    feat: add hierarchy sampling

--- backend/data/sampling.py
+++ backend/data/sampling.py
@@ -1,8 +1,12 @@
 import numpy as np
+import pickle
 import random
 import faiss
+import logging
 from sklearn.neighbors import NearestNeighbors
 from sklearn.decomposition import PCA
+
+logging.basicConfig(level=logging.DEBUG)
 
 # for efficient add, remove, and random select
 # modified from https://stackoverflow.com/questions/15993447/python-data-structure-for-efficient-add-remove-and-random-choice
@@ -31,54 +35,6 @@ class ListDict(object):
 
     def choose_random_item(self):
         return random.choice(self.items)
-
-class OutlierBiasedBlueNoiseSampling():
-    def __init__(self, sampling_rate, outlier_score=None, fail_rate=0.1):
-        self.sampling_rate = sampling_rate
-        self.outlier_score = outlier_score
-        self.fail_rate = fail_rate
-
-    def fit(self, data, category):
-        if self.outlier_score is None:
-            self.outlier_score = get_default_outlier_scores(data, category)
-        prob = self.outlier_score / (2 * np.max(self.outlier_score)) + 0.5
-
-        X = np.array(data.tolist(), dtype=np.float64)
-        n, d = X.shape
-        m = round(n * self.sampling_rate)
-        k = int(1 / self.sampling_rate)
-        neighbor, dist = Knn(X, n, d, k + 1, 1, 1, n)
-        radius = np.average(np.sqrt(dist[:, -1]))
-
-        selected_indexes = []
-
-        count = 0
-        candidates = ListDict(items=list(range(n)))
-        while count < m:
-            failure_tolerance = min(1000, (n - m) * self.fail_rate)
-            fail = 0
-            for i in range(len(candidates)):
-                idx = candidates.choose_random_item()
-                if fail > failure_tolerance or count >= m:
-                    break
-                if random.random() < prob[idx]:
-                    fail += 1
-                    continue
-                success = True
-                for selected_id in selected_indexes:
-                    if sum((data[idx] - data[selected_id])**2) < radius**2:
-                        success = False
-                        break
-                if success:
-                    count += 1
-                    selected_indexes.append(idx)
-                    candidates.remove_item(idx)
-                else:
-                    fail += 1
-            radius /= 2
-
-        selected_indexes = np.array(selected_indexes)
-        return selected_indexes
 
 class OutlierBiasedBlueNoiseSamplingFAISS():
     def __init__(self, sampling_rate, outlier_score=None, fail_rate=0.1):
@@ -134,8 +90,10 @@ class OutlierBiasedBlueNoiseSamplingFAISS():
             radius /= 2
 
         selected_indexes = np.array(selected_indexes)
-        return selected_indexes
-
+        _, nearestIdx = indexer.search(data, 1)
+        nearestIdx = np.array(selected_indexes)[nearestIdx.reshape((n))]
+        
+        return selected_indexes, nearestIdx
 
 def Knn(X, N, D, n_neighbors, forest_size, subdivide_variance_size, leaf_number):
     neighbors = NearestNeighbors(n_neighbors=n_neighbors, leaf_size=leaf_number)
@@ -157,43 +115,74 @@ def get_default_outlier_scores(data, category, k=50, dataIndexer = None):
     outlier_score = np.array(outlier_score) / k
     return outlier_score
 
+class HierarchySampling(object):
+    
+    def __init__(self):
+        super().__init__()
+        self.hierarchy = {}
+        self.max_depth = 0
+        self.top_nodes = []
+        
+    def fit(self, data, category, sampling_rate, top_nodes_count):
+        n = data.shape[0]
+        self.hierarchy = [[[]] for i in range(n)]
+        self.max_depth = 1
+            
+        selected_indexes = np.arange(n)
+        while len(selected_indexes)>top_nodes_count:
+            sampling_number = int(max(top_nodes_count, len(selected_indexes)*sampling_rate))
+            logging.debug("hierarchy sampling from {} to {}, depth = {}".format(len(selected_indexes), sampling_number, self.max_depth))
+            faiss_sampler = OutlierBiasedBlueNoiseSamplingFAISS(sampling_rate)
+            new_selected_indexes, neighbors = faiss_sampler.fit(data[selected_indexes], category[selected_indexes])
+            new_selected_indexes = selected_indexes[new_selected_indexes].tolist()
+            neighbors = selected_indexes[neighbors].tolist()
+            for new_selected_index in new_selected_indexes:
+                self.hierarchy[new_selected_index].append([])
+            self.max_depth += 1
+            for i in range(len(selected_indexes)):
+                self.hierarchy[neighbors[i]][self.max_depth-1].append(selected_indexes[i])
+            selected_indexes = np.array(new_selected_indexes)
+            
+        self.top_nodes = selected_indexes.tolist()
+        
+    def zoomin(self, indexes, depth):
+        if depth==0:
+            return self.top_nodes
+        else:
+            selected_indexes = []
+            depth += 1
+            assert depth<=self.max_depth
+            for index in indexes:
+                selected_indexes += self.hierarchy[index][self.max_depth-depth]
+            return selected_indexes
+        
+    def dump(self, path):
+        with open(path, "wb") as file:
+            pickle.dump({
+                "hierarchy": self.hierarchy,
+                "max_depth": self.max_depth,
+                "top_nodes": self.top_nodes
+                }, file)
+            
+    def load(self, path):
+        with open(path, "rb") as file:
+            hierarchyInfo = pickle.load(file)
+            self.hierarchy = hierarchyInfo["hierarchy"]
+            self.max_depth = hierarchyInfo["max_depth"]
+            self.top_nodes = hierarchyInfo["top_nodes"]
+            
 if __name__ == "__main__":
     from matplotlib import pyplot as plt
     from sklearn.manifold import TSNE
     import os
     import pickle
     import time
-    
-    # def tsne(X: np.ndarray, perplexity: int = 15, learning_rate: int = 3) -> np.ndarray:
-    #     X_embedded = TSNE(n_components=2, perplexity=perplexity, learning_rate=learning_rate).fit_transform(X)
-    #     return X_embedded
-    
-    # def _draw_tsne(X, labels, title):
-    #     label_colors = ["#A9A9A9", "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2",
-    #         "#ffdb45", "#bcbd22", "#17becf"]
-    #     colors = [label_colors[label] for label in labels]
-    #     plt.figure()
-    #     plt.scatter(X[:, 0], X[:, 1], s=2, c=colors)
-    #     plt.savefig('{}-tsne.png'.format(title))
-    
-    # random.seed(0)
-    # with open('/data/zhaowei/jittor-data/predict_info.pkl', 'rb') as f:
-    #     predictData = pickle.load(f)
-    # features = predictData['features']
-    # labels = predictData['labels']
-    # embedded_path = '/data/zhaowei/jittor-data/buffer/featuresEmbeded.npy'
-    # if os.path.exists(embedded_path):
-    #     embedded = np.load(embedded_path)
-    # else:
-    #     embedded = tsne(features)
-    #     np.save(embedded_path, embedded)
-    # indexes = np.load('/data/zhaowei/jittor-data/buffer/sample.npy')
-    # _draw_tsne(embedded, labels, 'all')
-    n = 100000
+
+    n = 5000
     features = np.random.rand(n, 2048)
     labels = np.random.randint(10, size=n)
     t0 = time.time()
-    sampler = OutlierBiasedBlueNoiseSamplingFAISS(0.1)
-    selectedIdxes = sampler.fit(features, labels)
+    sampler = HierarchySampling()
+    sampler.fit(features, labels, 0.5, 1000)
     print("sampling time", time.time()-t0)
-    np.save('/data/zhaowei/jittor-data/buffer/faiss_sample.npy', selectedIdxes)
+    sampler.dump('/data/zhaowei/jittor-data/buffer/faiss_sample.pkl')
